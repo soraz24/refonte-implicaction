be174be876f850a7d4717336b82544aaf0b852a1
SoLapsus <sofianegougam@lapsus.io>
2022-03-30 12:08:32 +0200

    add cypress in the project + add exmple of test in integration folder

--- frontend-implicaction/cypress/integration/sample_spec.js
+++ frontend-implicaction/cypress/integration/sample_spec.js
@@ -1,12 +1,15 @@
 describe("My First Test", () => {
   it("Visit implication", () => {
     cy.visit("http://localhost:8080/");
-    cy.contains("runner container");
+    cy.contains("type").click();
+    // Should be on a new URL which includes '/commands/actions'
+
+    cy.url().should("include", "/auth/sign-up");
   });
 });
 
-describe("Attempt to sign in", () => {
-  it("should have link to sign in", () => {
-    cy.get("http://localhost:8080/auth/sign-up").click();
-  });
-});
+// describe("Attempt to sign in", () => {
+//   it("should have link to sign in", () => {
+//     cy.get("http://localhost:8080/auth/sign-up").click();
+//   });
+// });
