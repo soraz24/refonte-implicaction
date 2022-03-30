ae05cb3aeb31b7d6df06066ae8ab71c436874e2d
SoLapsus <sofianegougam@lapsus.io>
2022-03-30 13:28:06 +0200

    add cypress

--- frontend-implicaction/cypress/integration/sample_spec.js
+++ frontend-implicaction/cypress/integration/sample_spec.js
@@ -8,6 +8,16 @@ describe("My First Test", () => {
   });
 });
 
+describe("My second Test", () => {
+  it("Visit implication", () => {
+    cy.visit("http://localhost:8080/");
+    cy.contains("type").click();
+    // Should be on a new URL which includes '/commands/actions'
+
+    cy.url().should("include", "/auth/sign-up");
+  });
+});
+
 // describe("Attempt to sign in", () => {
 //   it("should have link to sign in", () => {
 //     cy.get("http://localhost:8080/auth/sign-up").click();
